99bbe03b8bc0151dcf68eeb1f974037acb5b4513
zzombie1312 <50015657+zzombie1312@users.noreply.github.com>
2019-05-13 11:29:45 +0900

    Initial commit

---
+++ DataManager.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileDataInputOutput
+{
+    class DataManager
+    {
+        private String Location;
+
+        public static List<string> listOfDatas = new List<string>();
+        public DataManager(String Location)
+        {
+            this.Location = Location;
+            listOfDatas.Clear();
+        }
+
+        public void readAllAddress()
+        {
+            using (StreamReader reader = new StreamReader(Location))
+            {
+                string current;
+                while ((current = reader.ReadLine()) != null)
+                {
+                    if (current.StartsWith("") && current.EndsWith(":"))
+                    {
+                        listOfDatas.Add(current.Substring(0, current.Length -1));
+                    }
+                }
+            }
+        }
+
+        public void Append(String name, String subAddress, String[] data)
+        {
+
+            List<String> datas = new List<string>();
+            using (StreamReader reader = new StreamReader(Location))
+            {
+                String current;
+                while ((current = reader.ReadLine()) != null)
+                {
+                    if (current.Equals(name + ":"))
+                    {
+                        datas.Add(name + ":");
+                        datas.Add("\t-" + subAddress);
+                        foreach (string s in data)
+                            datas.Add("\t\t-" + s);
+                        while ((current = reader.ReadLine()) != null)
+                            if (current.StartsWith("-"))
+                                break;
+                    }
+
+                    datas.Add(current);
+                }
+            }
+            using(StreamWriter writer = new StreamWriter(Location)) { 
+                foreach (String s in datas)
+                {
+                    writer.WriteLine(s);
+                }
+            }
+        }
+        //Is Not Contains
+        public void NewAddress(String name, String subAddress, String[] data)
+        {
+            using (StreamWriter writer = new StreamWriter(Location, true))
+            {
+                writer.WriteLine(name + ":");
+                writer.WriteLine("\t-" + subAddress);
+                foreach (string s in data)
+                {
+                    writer.WriteLine("\t\t-" + s);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Common;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace FileDataInputOutput
+{
+    class FileData
+    {
+        private readonly string LOC = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\FileD.dat";
+
+        private DataManager dataManager;
+        /*
+         * initialization
+         */
+        public FileData()
+        {
+            if(!File.Exists(LOC)) File.Create(LOC).Close();
+            dataManager = new DataManager(LOC);
+            dataManager.readAllAddress();
+        }
+
+        public void Write(String name, String subName, String[] data)
+        {
+            if(!isIn(name, DataManager.listOfDatas))
+                dataManager.NewAddress(name, subName, data);
+            else dataManager.Append(name, subName, data);
+        }
+
+        private bool isIn(string name, List<String> datas)
+        {
+            foreach (String s in datas)
+            {
+                if (s == name) return true;
+            }
+
+            return false;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileData data = new FileData();
+            data.Write("Hello", "Bye", new string[3]{"Bye", "World", "Hello"});
+
+            data.Write("Hell", "Bye", new string[3] { "Bye", "World", "Hello" });
+
+            data.Write("Hell", "asg", new string[3] { "Bye", "World", "Hello" });
+
+            foreach (String s in DataManager.listOfDatas)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
